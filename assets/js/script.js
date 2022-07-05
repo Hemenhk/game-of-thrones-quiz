581b988f555926db564a359814a5ce1d87207048
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-07-05 18:00:48 +0000

    feat: Add five const in js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const question = document.getElementById('#quiz-quesiton');
+const choices = document.Array.from(document.querySelectorAll('.choice-text'));
+const question = document.getElementById('#quiz-quesiton');
+const question = document.getElementById('#quiz-quesiton');
+const question = document.getElementById('#quiz-quesiton');
