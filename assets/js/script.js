fc675ad9e6d249ffea153bc028b29b80fe49f7ff
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-07-05 18:12:10 +0000

    feat: Create first question for the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,19 @@ const choices = document.Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.getElementById('#progressText');
 const score = document.getElementById('#score');
 const progressBarFull = document.getElementByClassName('.progressBarFull');
+
+let question = {};
+let acceptedAnswers = true;
+let score = 0;
+let questionCounting = 0;
+let remainingQuestions = [];
+
+let questions = [
+    {
+        question: 'What is the seat of house Tully?',
+        choice1: 'Winterfell',
+        choice2: 'Casterly Rock',
+        choice3: 'Riverrun',
+        choice4: 'The Eyrie'
+    }
+]
